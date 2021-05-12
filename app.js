2adbb8d64422532180224a5f3594c271cf341644
Joel Ogunremi <joelogunremi@gmail.com>
2021-05-11 21:15:11 -0400

    index route render home page

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { projects } = require("./data.json");
+const path = require("path");
 
-app.listen(port);
+//Middleware
+app.set("view engine", "pug");
+// Serving the static files located in the public folder
+app.use("/static", express.static("public"));
+// Routes
+app.get("/", (req, res) => {
+  // render the pug template, pass pug the projects object
+  // the second parameter is the local set  to view the data object
+  res.render("index", { projects });
+});
+
+app.listen(port, () => {
+  console.log("The application is running on localhost:3000!");
+});
