c312a73e1cf713c3e0df27a197b732d2f9dd6926
Joel Ogunremi <joelogunremi@gmail.com>
2021-05-13 16:25:54 -0400

    error handling added

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   // render the about pug template, pass pug the projects object
+  //res.status(500).render("error", { err });
   res.render("about");
 });
 
@@ -25,6 +26,28 @@ app.get("/projects/:id", (req, res, next) => {
   // id is a variable and render different projects
   const projectId = req.params.id;
   res.render("project", { project: projects[projectId] });
+});
+// 404 Error Handler
+app.use(function (req, res, next) {
+  console.log("404 error handler called");
+  const err = new Error();
+  err.status = 404;
+  err.message =
+    "Oops!  It looks like the page you're looking for does not exist.";
+  res.status(404).render("page-not-found", { err });
+  //next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    console.log("404 error handler called");
+    res.status(404).render("page-not-found", { err });
+  } else {
+    console.log("500 error handler called");
+    err.message = "Oops, something went wrong.";
+    res.status(err.status || 500).render("error", { err });
+  }
+  console.log(err.message);
 });
 
 app.listen(port, () => {
